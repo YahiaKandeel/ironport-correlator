d94b91da1ff2a06461fdcdc26a1332d02d885847
YahiaKandeel <yahia.kandeel@gmail.com>
2019-05-05 18:32:18 +0300

    Fixing Bugs in the Subject decoding

--- correlator/lib/decoder.py
+++ correlator/lib/decoder.py
@@ -1,13 +1,31 @@
 from email.header import decode_header
-import codecs
 
 
 def decode(text):
-    result = []
+    blacklist = ['\r\n', '\\r\\n', '\\\r\\\n', '\\\\r\\\\n']
     items = decode_header(text)
+    result = []
+
+    # ITerating Over Items
     for string, encoding in items:
-        if encoding:
-            result.append(codecs.decode(string, encoding))
-        elif not encoding and type(string) != bytes:
-            result.append(str(string))
-    return ''.join(result)
+        try:
+            if encoding:
+                string = str(string, encoding).encode().decode()
+                # print('\r\n---------------', string, encoding,'--------------')
+                result.append(string)
+            elif not encoding and type(string) != bytes:
+                # print('\r\n####################', string, encoding,'####################')
+                result.append(str(string))
+        except Exception as e:
+            print("\r\n\ISUSEEEEE-------->", text, str(e),'--------------\r\n\r\n')
+            result = [text]
+
+    # Construct Result, Removing Bad Chars
+    result = ' '.join(result).strip()
+    for badchrs in blacklist:
+        result = result.replace(badchrs,'')
+
+    if result:
+        print('\r\n\r\n####################', result, '####################')
+
+    return result
--- correlator/lib/logger.py
+++ correlator/lib/logger.py
@@ -4,22 +4,29 @@
 from logging.handlers import SysLogHandler
 import logging
 import json
+import pprint
 import time
-from decoder import decode
+from .decoder import decode
 import collections
 
 
 # Syslog
-def syslog(server, port=514, ident='IronPort'):
-    address = (server, port)
+def syslog(server=None, local_file=None, port=514, ident='IronPort'):
     facility = SysLogHandler.LOG_LOCAL5
     # Logger
     log = logging.getLogger(ident)
     log.setLevel(logging.INFO)
+
     # add handler to the logger
-    handler = SysLogHandler(address=address, facility=facility)
-    # add formatter to the handler
-    formatter = logging.Formatter('%(name)s: %(message)r')
+    if server:
+        handler = SysLogHandler(address=(server, port), facility=facility)
+        formatter = logging.Formatter('%(name)s: %(message)r')
+    elif local_file:
+        handler = logging.FileHandler(local_file, encoding='utf-8')
+        formatter = logging.Formatter("%(asctime)s %(name)s: %(message)s",
+                                    "%Y-%m-%d %H:%M:%S")
+    else:
+        return False
     handler.setFormatter(formatter)
     log.addHandler(handler)
     # return
@@ -32,35 +39,39 @@ def style(message):
     message_log = collections.OrderedDict()
 
     # Order
-    keys = ['ICID', 'MID' , "MessageID", 'NewMID', 'OldMID',
-            # 'Rewrite_By', 'Generated_By', 'Generated_By_Filter'
+    keys = ['ICID', 'MID' , "MessageID", 'Related_MID',
             'OutbreakFilters', 'CASE', 'GRAYMAIL', 'Antivirus', 'LDAP_Drop',
             'SPF' ,'DKIM', 'DKIM_Detail', 'DMARK', 'DMARK_Detail',
             "Subject", "Attachments", "From", "To",
             "SenderReputation", "ThreatCategory", "SuspectedDomains", "DomainAge",
-            'Action', 'Action_Desc', 'Action_Filter', "IP"
+            'Action', 'Action_Desc', 'Content_Filter', "IP",
             "Other"]
 
     for key in keys:
-        message_log[key] = ' || '.join(list(set(message.get(key, []))))
+        values = filter(None, message.get(key, []))
+        message_log[key] = ' || '.join(list(set(values)))
 
     # Decode Subject
     message_log["Subject"] = decode(message_log["Subject"])
-    message_log["Attachments"] = decode(message_log["Attachments"])
+    # message_log["Attachments"] = decode(message_log["Attachments"])
 
     for recipient in recipients:
         message_log['To'] = recipient
-        result.append(json.dumps(message_log))
+        result.append(json.dumps(message_log, ensure_ascii = False))
 
     return result
 
 
-def logger(logger_queue, syslog_server):
+def logger(logger_queue, syslog_server=None, local_file=None):
 
-    print "\t[+]Starting Logger Process"
+    print ("\t[+]Starting Logger Process")
 
     # Logger
-    log = syslog(syslog_server)
+    if syslog_server:
+        log = syslog(server=syslog_server)
+
+    if local_file:
+        local_log = syslog(local_file=local_file)
 
     while True:
 
@@ -74,9 +85,11 @@ def logger(logger_queue, syslog_server):
 
             # Log
             for message in messages:
-                log.info(message)
-                print message
-
+                if syslog_server:
+                    log.info(message)
+                if local_file:
+                    local_log.info(message)
+                pprint.pprint(json.loads(message))
         else:
             # sleep
             time.sleep(0.05)
