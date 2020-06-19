ff8170cb219079e720cdd4edb47dd7f39528c61a
Yahia Kandeel <yahia.kandeel@gmail.com>
2020-06-19 10:35:20 +0200

    Adding correlator service
    	- Updating Logstash
    	- Restructing Install.sh

--- install.sh
+++ install.sh
@@ -2,38 +2,46 @@
 ################################################################################
 # Logstash, Redis, Python3 Installation Bash Script on CentOS7
 ################################################################################
+
+###########
 # Bootstrap
+###########
+# Installation
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum install -y wget vim firewalld net-tools gcc make openssl-devel
-# Start Firewalld
-systemctl mask iptables
-systemctl enable firewalld --now
 
+###########
 # Logstash
+###########
 yum install -y java-1.8.0-openjdk
-rpm -ivh https://artifacts.elastic.co/downloads/logstash/logstash-7.1.0.rpm
-# copy logstash config file
-if [ -e /vagrant/logstash.conf ]; then 
+rpm -ivh https://artifacts.elastic.co/downloads/logstash/logstash-7.8.0.rpm
+
+# Config
+[ -e /vagrant/logstash.conf ] && \
 	cp /vagrant/logstash.conf /etc/logstash/conf.d/ironport.conf
-else
-	cp ./logstash.conf /etc/logstash/conf.d/ironport.conf
-fi
-# Start the services
+
+# Start & Enable
 systemctl enable logstash --now
 
+###########
 # Redis
+###########
 yum install -y redis
 # Start the services
 systemctl enable redis --now
 
-# Python
+###########
+# Python3
+###########
 yum -y install python36 python36-devel
 yum -y install python36-pip
 pip3 install ipython redis
 
-# Configure logrotate
+###########
+# logrotate
+###########
 mkdir /var/log/ironport/
-echo '''/var/log/ironport/*log {
+echo '''/var/log/ironport/*.log {
     daily
     rotate 10
     copytruncate
@@ -41,10 +49,29 @@ echo '''/var/log/ironport/*log {
 }
 ''' > /etc/logrotate.d/ironport
 
-# Firewall Configurations
-firewall-cmd --add-port=514/tcp --permanent
+###########
+# Firewalld
+###########
+systemctl mask iptables
+systemctl enable firewalld --now
 firewall-cmd --add-port=514/udp --permanent
 firewall-cmd --reload
 
-# Make it as a service
-# Run it permenently ;)
+###########
+# Systemd
+###########
+echo '''[Unit]
+Description=Ironport Correlator Service
+After=network.target
+
+[Service]
+Type=simple
+User=vagrant
+ExecStart=/bin/python3 /vagrant/main.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target''' > /etc/systemd/system/ironport.service
+
+systemctl daemon-reload
+systemctl enable --now ironport
